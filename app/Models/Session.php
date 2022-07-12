590f17edccad40f07bfa5a48833b3a547e66e5de
miiajj <84679284+miiajj@users.noreply.github.com>
2022-07-12 15:04:30 +0700

    vendor: add laravel permission, laravel passport

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Session extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function bids(): HasMany
+    {
+        return $this->hasMany(Bid::class);
+    }
+
+    public function product(): 
 }
