c50fb53488addffe22cc183b8ae97b71aa5ea4a7
nguyenbinhminh99 <104883335+nguyenbinhminh99@users.noreply.github.com>
2022-07-19 16:13:48 +0700

    Delete test-bid.blade.php

--- resources/views/test-bid.blade.php
+++
@@ -1,8 +0,0 @@
-<form action="/bid" method="post">
-    <b>Product  </b> <input type="text" name="name" placeholder="product name"><br><br>
-    <b>Price  </b><input type="text" name="price" placeholder="price"><br><br>
-    <b>Auction  </b><input type="text" name="auction" placeholder="auction"><br><br>
-    <b>Session  </b><input type="text" name="session" placeholder="session"><br><br>
-    <button type="submit">Bid</button>
-    {{ csrf_field() }}
-</form>
