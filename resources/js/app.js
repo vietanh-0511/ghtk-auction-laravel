e05ee9cdef65e00d2c28e5744047c860e43634b7
Cao Anh <anhcx0209@gmail.com>
2022-07-20 13:54:12 +0700

    frontend init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
+import Layout from "./Layout";
+
 require('./bootstrap');
+import {Routes, Route, HashRouter} from "react-router-dom";
+import ReactDOM from "react-dom";
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
+
+if (document.getElementById('root')) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+    document.getElementById('root'));
+}
