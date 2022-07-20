e9ce866c449ccc74c18e0692eebab7ba5cee1d8c
Tran-Tuan-Hiep <trantuanhiep07022001@gmail.com>
2022-07-20 10:00:50 +0700

    update route bid

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 //User 
 Route::controller(UserController::class)->group(function () {
     Route::post('/login', 'login');
