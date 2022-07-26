b16e265733c0fda133e8b1b5bd10532033d4d9be
nguyenbinhminh99 <minhmocmeo0@gmail.com>
2022-07-26 17:52:01 +0700

    edit seeders

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -14,11 +14,23 @@ class ProductFactory extends Factory
      */
     public function definition()
     {
+        $name = array('Orbital Keys', 'XPress Bottle', 'InstaPress', 'Uno Wear', 'Allure Kit', 'Swish Wallet',
+                        'Onovo Supply', 'Sharpy Knife', 'Towlee', 'Rhino Case', 'Mono', 'Handy Mop',
+                        'ONEset', 'Vortex Bottle', 'Terra Shsave', 'Gym Kit', 'Villafy', 'Stickem',
+                        'Snap It', 'Scruncho'
+                );
+        $price = array(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1200, 2000, 3000);
+        $description = array('This product has a good rating',
+                            'Great choice',
+                            'Best sellers',
+                            'The right choice',
+                            'This product is highly rated by experts'
+                        );
         return [
-            'name' => $this->faker->name(),
-            'desirable_price' => Str::random(3),
+            'name' => $name[array_rand($name)],
+            'desirable_price' => $price[array_rand($price)],
             'title_image' => 'titleimage',
-            'description' => 'descriptionforproduct',
+            'description' => $description[array_rand($description)],
         ];
     }
 }
