4133991f61a18682dcad4730135060ab8ddf31b9
miiajj <84679284+miiajj@users.noreply.github.com>
2022-07-29 14:17:00 +0700

    remove func adminLogin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
   public function __construct()
   {
-    $this->middleware('auth:api', ['except' => ['login', 'adminLogin', 'register']]);
+    $this->middleware('auth:api', ['except' => ['login', 'register']]);
   }
 
   public function login(LoginRequest $request)
