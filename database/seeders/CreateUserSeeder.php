ae0d1dd4fc8af7d06dc7eafde22dad0e1f7b3cc9
nguyenbinhminh99 <minhmocmeo0@gmail.com>
2022-08-05 09:40:11 +0700

    1

--- database/seeders/CreateUserSeeder.php
+++ database/seeders/CreateUserSeeder.php
@@ -8,7 +8,6 @@ use Spatie\Permission\Models\Role;
 
 class CreateUserSeeder extends Seeder
 {
-<<<<<<< Updated upstream
     /**
      * Run the database seeds.
      *
@@ -16,34 +15,32 @@ class CreateUserSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
-            'full_name' => 'user',
-            'email' => 'user.auctions@gmail.com',
-            'password' => 'User@123123',
-            'address' => 'Hanoi',
-            'phone' => '0857694721'
-        ]);
+      $user = User::create([
+        'full_name' => 'user',
+        'email' => 'user.auctions@gmail.com',
+        'password' => 'User@123123',
+        'address' => 'Hanoi',
+        'phone' => '0857694721'
+      ]);
+    }
 
-=======
-<<<<<<< HEAD
-  /**
-   * Run the database seeds.
-   *
-   * @return void
-   */
-  public function run()
-  {
-    $user = User::create([
-      'full_name' => 'user',
-      'email' => 'user.auctions@gmail.com',
-      'password' => 'User@123123',
-      'address' => 'Hanoi',
-      'phone' => '0857694721'
-    ]);
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $user = User::create([
+        'full_name' => 'user',
+        'email' => 'user.auctions@gmail.com',
+        'password' => 'User@123123',
+        'address' => 'Hanoi',
+        'phone' => '0857694721'
+      ]);
 
     $user->assignRole('user');
-  }
-=======
+    }
     /**
      * Run the database seeds.
      *
@@ -59,7 +56,6 @@ class CreateUserSeeder extends Seeder
             'phone' => '0857694721'
         ]);
 
->>>>>>> Stashed changes
         $user1 = User::create([
             'full_name' => 'user1',
             'email' => 'user1.auctions@gmail.com',
@@ -80,8 +76,4 @@ class CreateUserSeeder extends Seeder
         $user1->assignRole('user');
         $admin->assignRole('admin');
     }
-<<<<<<< Updated upstream
-=======
->>>>>>> 9ded1004480fe81c9c0c7bbe91b10c8b2352c93a
->>>>>>> Stashed changes
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,20 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 //         \App\Models\User::factory(2)->create();
-=======
->>>>>>> Stashed changes
         // \App\Models\User::factory(10)->create();
         \App\Models\Product::factory(0)->create();
         \App\Models\User::factory(0)->create();
 
-<<<<<<< Updated upstream
-=======
->>>>>>> 9ded1004480fe81c9c0c7bbe91b10c8b2352c93a
->>>>>>> Stashed changes
         $this->call([
             RoleTableSeeder::class,
             PermissionTableSeeder::class,
